6e9544b4aa803ce5d1409ad71f42726207145b46
figraham <fi.sage.graham@gmail.com>
2019-07-25 20:10:03 -0600

    game board created

--- 3dtic.js
+++ 3dtic.js
@@ -4,38 +4,52 @@ let canvas;
 let backgroundColor;
 
 let board;
+let cam;
 
 function preload() {
 
 }
 
 function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
+  canvas = createCanvas(windowWidth, windowHeight, WEBGL);
   backgroundColor = color(12, 12, 56);
+
+  cam = createCamera();
+  cam.ortho(-width / 2, width / 2, height / 2, -height / 2, -500, 1000);
+
   rectMode(CENTER);
   noFill();
   stroke(255);
   strokeWeight(3);
 
-  board = new GameBoard(4, 4, 4);
+  board = new GameBoard(4, 4, 4, 50);
 }
 
 function windowResized() {
   canvas.resize(windowWidth, windowHeight);
+  cam.ortho(-width / 2, width / 2, height / 2, -height / 2, -500, 1000);
 }
 
 function draw() {
   background(backgroundColor);
+
+  cam.setPosition(
+    cos(frameCount * 0.001),
+    map(mouseY, 0, height, PI/5, PI/7),
+    sin(frameCount * 0.001)
+  );
+  cam.lookAt(0, 0, 0);
 
   board.draw();
 }
 
 class GameBoard {
 
-  constructor(width, height, depth) {
+  constructor(width, height, depth, scale) {
     this.width = width;
     this.height = height;
     this.depth = depth;
+    this.scale = scale;
     this.clear();
     console.log(this);
   }
@@ -48,12 +62,18 @@ class GameBoard {
   }
 
   draw() {
-    let w = width / this.width;
-    let h = height / this.height;
-    for (let i = 0; i < this.height; i++) {
-      let x = width / 2;
-      let y = h / 2 + (i * h);
-      square(x, y, h);
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.height; j++) {
+        for (let k = 0; k < this.depth; k++) {
+          push();
+          let x = ((i + .5) - (this.width / 2)) * this.scale;
+          let y = ((j + .5) - (this.height / 2)) * this.scale * 3;
+          let z = ((k + .5) - (this.depth / 2)) * this.scale;
+          translate(x, y, z);
+          box(this.scale, 0, this.scale);
+          pop();
+        }
+      }
     }
   }
 
